c95c87566fbd7bb9a863ef334551a6a20220e399
vain0x <vainzerox@gmail.com>
2025-06-21 16:56:04 +0900

    chore: Add dependencies, update config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite'
+import vuetify from 'vite-plugin-vuetify'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    // https://vuetifyjs.com/
+    // https://www.npmjs.com/package/vite-plugin-vuetify
+    vuetify({ autoImport: true }),
+  ],
 })
