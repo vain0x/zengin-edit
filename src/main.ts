704b2547ad9f0d9243565c48c6e36c6aefec32b1
vain0x <vainzerox@gmail.com>
2025-06-22 16:50:09 +0900

    chore: Setup svg icons

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+import { aliases, mdi } from 'vuetify/iconsets/mdi-svg'
 import 'vuetify/styles/main.css'
 import App from './App.vue'
 import './style.css'
@@ -9,6 +10,13 @@ import './style.css'
 const vuetify = createVuetify({
   components,
   directives,
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+      mdi,
+    },
+  },
 })
 
 createApp(App).use(vuetify).mount('#app')
